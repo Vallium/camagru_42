9b1a8bebfdcb6277b14c3b22b2a508feed67b071
Vallium <aalliot@student.42.fr>
2016-01-15 00:16:25 +0100

    add models

---
+++ model/ImageModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace model;
+
+class ImageModel
+{
+    static function createTable(\PDO $db, $schema = "camagru")
+    {
+
+    }
+}
---
+++ model/UserModel.php
@@ -0,0 +1,33 @@
+<?php
+namespace model;
+
+class UserModel
+{
+    private $db;
+
+    public function findById($id)
+    {
+        $req = 'SELECT * FORM users WHERE id='.(int)$id;
+        $r = $this->db->query($req);
+        return $r->fetchColumn();
+    }
+
+    static function createTable(\PDO $db, $schema = "camagru")
+    {
+        //Drop
+        $req = "DROP TABLE IF EXISTS $schema.users";
+        $db->exec($req);
+
+        //Create
+        $req = "CREATE TABLE IF NOT EXISTS $schema.users(
+                      id INT NOT NULL AUTO_INCREMENT,
+                      username VARCHAR(45) NULL,
+                      password VARCHAR(255) NULL,
+                      email VARCHAR(255) NULL,
+                      is_admin TINYINT(1) NULL DEFAULT 0,
+                      PRIMARY KEY (id),
+                      UNIQUE INDEX username_UNIQUE (username ASC),
+                      UNIQUE INDEX email_UNIQUE (email ASC))";
+        return $db->exec($req);
+    }
+}
