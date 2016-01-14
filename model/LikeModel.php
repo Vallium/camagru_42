19dd701819bd0b2902d96224bb47b8ce4da1f857
Vallium <aalliot@student.42.fr>
2016-01-15 00:36:06 +0100

    LikeModel

---
+++ model/LikeModel.php
@@ -0,0 +1,32 @@
+<?php
+namespace model;
+
+class LikeModel
+{
+    static function createTable(\PDO $db, $schema = "camagru")
+    {
+        //Drop
+        $req = "DROP TABLE IF EXISTS $schema.likes";
+        $db->exec($req);
+
+        //Create
+        $req = "CREATE TABLE IF NOT EXISTS $schema.likes (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    images_id INT NOT NULL,
+                    users_id INT NOT NULL,
+                    PRIMARY KEY (id),
+                    INDEX fk_notes_images1_idx (images_id ASC),
+                    INDEX fk_notes_users1_idx (users_id ASC),
+                    CONSTRAINT fk_notes_images1
+                    FOREIGN KEY (images_id)
+                    REFERENCES $schema.images (id)
+                    ON DELETE CASCADE
+                    ON UPDATE NO ACTION,
+                    CONSTRAINT fk_notes_users1
+                    FOREIGN KEY (users_id)
+                    REFERENCES $schema.users (id)
+                    ON DELETE NO ACTION
+                    ON UPDATE NO ACTION)";
+        return $db->exec($req);
+    }
+}
