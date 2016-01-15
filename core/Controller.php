cee6e2f5e2e9a7fc1229e58943ed01aa1cf1cfc9
Vallium <aalliot@student.42.fr>
2016-01-15 21:01:59 +0100

    move in core

---
+++ core/Controller.php
@@ -0,0 +1,30 @@
+<?php
+namespace controller;
+
+abstract class Controller
+{
+    private $vars = array();
+    private $layout = 'default';
+
+    abstract public function index();
+
+    public function set(array $var)
+    {
+        $this->vars = array_merge($this->vars, $var);
+    }
+
+    public function render($view)
+    {
+        extract($this->vars);
+
+        ob_start();
+        require (ROOT.'views'.DS.$view);
+        $content_for_layout = ob_get_clean();
+
+        if ($this->layout == false)
+            echo $content_for_layout;
+        else
+            require ROOT.'views'.DS.'layout'.DS.$this->layout.'.php';
+    }
+
+}
