9ea8b78675f6c2de54a13261fa954a9fe78224df
Vallium <anton.alliot@gmail.com>
2016-03-23 18:04:13 +0100

    delete sction and article

--- views/signin.php
+++ views/signin.php
@@ -5,25 +5,21 @@
 </head>
 <body>
     <div class="holder">
-        <section>
-            <article>
-                <h1>SIGN-IN</h1>
-                <form method="post" action="">
-                    <div>
-                        <label>USERNAME</label>
-                        <input type="text" name="username" autofocus>
-                    </div>
-                    <div>
-                        <label>PASSWORD</label>
-                        <input type="password" name="password">
-                    </div>
-                    <div class="toSubscribe">
-                        <a href="/user/signup">If you don't have an account, you can sign-up now</a>
-                        <a href="#">Forgot your password? Send me an email</a>
-                    </div>
-                    <button class="button orange">CONNECT</button>
-                </form>
-            </article>
-        </section>
+        <h1>SIGN-IN</h1>
+        <form method="post" action="">
+            <div>
+                <label>USERNAME</label>
+                <input type="text" name="username" autofocus>
+            </div>
+            <div>
+                <label>PASSWORD</label>
+                <input type="password" name="password">
+            </div>
+            <div class="toSubscribe">
+                <a href="/user/signup">If you don't have an account, you can sign-up now</a>
+                <a href="#">Forgot your password? Send me an email</a>
+            </div>
+            <button class="button orange">CONNECT</button>
+        </form>
     </div>
 </body>
--- views/signup.php
+++ views/signup.php
@@ -5,29 +5,25 @@
 </head>
 <body>
     <div class="holder">
-        <section>
-            <article>
-                <h1>SIGN-UP</h1>
-                <form method="post" action="">
-                    <div>
-                        <label>USERNAME</label>
-                        <input type="text" name="username" autofocus>
-                    </div>
-                    <div>
-                        <label>EMAIL</label>
-                        <input type="email" name="email">
-                    </div>
-                    <div>
-                        <label>PASSWORD</label>
-                        <input type="password" name="password">
-                    </div>
-                    <div>
-                        <label>CONFIRM PASSWORD</label>
-                        <input type="password" name="passwordConf">
-                    </div>
-                    <button class="button orange">SUBSCRIBE</button>
-                </form>
-            </article>
-        </section>
+        <h1>SIGN-UP</h1>
+        <form method="post" action="">
+            <div>
+                <label>USERNAME</label>
+                <input type="text" name="username" autofocus>
+            </div>
+            <div>
+                <label>EMAIL</label>
+                <input type="email" name="email">
+            </div>
+            <div>
+                <label>PASSWORD</label>
+                <input type="password" name="password">
+            </div>
+            <div>
+                <label>CONFIRM PASSWORD</label>
+                <input type="password" name="passwordConf">
+            </div>
+            <button class="button orange">SUBSCRIBE</button>
+        </form>
     </div>
 </body>
