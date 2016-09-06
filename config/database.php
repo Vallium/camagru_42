113c02808825bfd48fafcdc63c512380bde63cc1
Vallium <anton.alliot@gmail.com>
2016-09-06 16:52:12 +0200

    last commits

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
-    $DB_DSN = 'mysql:host=127.0.0.1;dbname=camagru';
+    $DB_SCHEMA = 'camagru';
+
+    $DB_DSN = "mysql:host=127.0.0.1;dbname=$DB_SCHEMA";
     $DB_DSN_SETUP = 'mysql:host=127.0.0.1';
 
     $DB_USER = 'camagru';
--- config/setup.php
+++ config/setup.php
@@ -3,9 +3,7 @@
     ini_set('display_errors', 'on');
     include('database.php');
 
-    $schema = 'camagru';
-
-    echo 'Initialisation of Camagru website';
+    echo 'Initialisation of Camagru website'.PHP_EOL;
 
     try {
         $pdo = new PDO($DB_DSN_SETUP, $DB_USER, $DB_PASSWORD);
@@ -15,9 +13,15 @@
 //        -- -----------------------------------------------------
 //        -- Schema camagru
 //        -- -----------------------------------------------------
-        $req = "DROP SCHEMA IF EXISTS $schema";
+        $req = "DROP SCHEMA IF EXISTS $DB_SCHEMA";
         $pdo->exec($req);
-        $req = "CREATE SCHEMA IF NOT EXISTS $schema DEFAULT CHARACTER SET utf8";
+        $req = "CREATE SCHEMA IF NOT EXISTS $DB_SCHEMA DEFAULT CHARACTER SET utf8";
+        $pdo->exec($req);
+        $req = 'SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0';
+        $pdo->exec($req);
+        $req = 'SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0';
+        $pdo->exec($req);
+        $req = "SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES'";
         $pdo->exec($req);
 
         $root = dirname(__DIR__);
@@ -27,27 +31,34 @@
 //        -- Table camagru.users
 //        -- -----------------------------------------------------
         require $root.'/models/UserModel.php';
-        \model\UserModel::createTable($pdo, $schema);
+        \model\UserModel::createTable($pdo, $DB_SCHEMA);
 
 //        -- -----------------------------------------------------
 //        -- Table camagru.images
 //        -- -----------------------------------------------------
         require $root.'/models/ImageModel.php';
-        \model\ImageModel::createTable($pdo, $schema);
+        \model\ImageModel::createTable($pdo, $DB_SCHEMA);
 
 //        -- -----------------------------------------------------
 //        -- Table camagru.likes
 //        -- -----------------------------------------------------
         require $root.'/models/LikeModel.php';
-        \model\LikeModel::createTable($pdo, $schema);
+        \model\LikeModel::createTable($pdo, $DB_SCHEMA);
 
 //        -- -----------------------------------------------------
 //        -- Table camagru.comments
 //        -- -----------------------------------------------------
         require $root.'/models/CommentModel.php';
-        \model\CommentModel::createTable($pdo, $schema);
+        \model\CommentModel::createTable($pdo, $DB_SCHEMA);
 
-        echo 'Database initialized with success';
+        $req = 'SET SQL_MODE=@OLD_SQL_MODE';
+        $pdo->exec($req);
+        $req = 'SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS';
+        $pdo->exec($req);
+        $req = 'SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS';
+        $pdo->exec($req);
+
+        echo 'Database initialized with success'.PHP_EOL;
 
         $files = glob($root.'/web/img/uploads/*'); // get all file names
         foreach($files as $file){ // iterate files
@@ -55,7 +66,8 @@
                 unlink($file); // delete file
         }
 
-        echo 'All uploaded files have been deleted with success';
+        echo 'All uploaded files have been deleted with success'.PHP_EOL;
+        echo 'Website deployed with success'.PHP_EOL;
 
     } catch (PDOException $e) {
         echo 'Connection failed: '.$e->getMessage();
